65a8e024fb130f7dda0dffd5a3327465c95ae12e
Raineri <asa.raineri20@gmail.com>
2024-11-30 20:20:34 -0300

    concluido

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,67 +1,79 @@
-import { useNavigate  } from "react-router-dom";
-import { MdEmail, MdLock } from 'react-icons/md'
-import { Button } from '../../components/Button';
-import { Header } from '../../components/Header';
-import { Input } from '../../components/Input';
-import { api } from '../../services/api';
-
-import { useForm } from "react-hook-form";
-
-
-import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
-
 const Login = () => {
+    const navigate = useNavigate();
 
-    const navigate = useNavigate()
-
-    const { control, handleSubmit, formState: { errors  } } = useForm({
+    const { control, handleSubmit, formState: { errors } } = useForm({
         reValidateMode: 'onChange',
         mode: 'onChange',
     });
 
     const onSubmit = async (formData) => {
-        try{
-            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
+        try {
+            const { data } = await api.post('/login', { email: formData.email, senha: formData.senha });
             
-            if(data.length && data[0].id){
-                navigate('/feed') 
-                return
+            if (data.length && data[0].id) {
+                navigate('/feed');
+                return;
             }
 
-            alert('Usuário ou senha inválido')
-        }catch(e){
-            //TODO: HOUVE UM ERRO
+            alert('Usuário ou senha inválido');
+        } catch (e) {
+            console.error('Erro ao realizar login:', e);
+            alert('Ocorreu um erro ao tentar realizar o login. Tente novamente mais tarde.');
         }
     };
 
-    console.log('errors', errors);
+    const handleCreateAccount = () => navigate('/signup');
 
-    return (<>
-        <Header />
-        <Container>
-            <Column>
-                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
-                 e entrar mais rápido nas empresas mais desejadas.</Title>
-            </Column>
-            <Column>
-                <Wrapper>
-                <TitleLogin>Faça seu cadastro</TitleLogin>
-                <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
-                    {errors.email && <span>E-mail é obrigatório</span>}
-                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
-                    {errors.senha && <span>Senha é obrigatório</span>}
-                    <Button title="Entrar" variant="secondary" type="submit"/>
-                </form>
-                <Row>
-                    <EsqueciText>Esqueci minha senha</EsqueciText>
-                    <CriarText>Criar Conta</CriarText>
-                </Row>
-                </Wrapper>
-            </Column>
-        </Container>
-    </>)
-}
+    return (
+        <>
+            <Header />
+            <Container>
+                <Column>
+                    <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
+                     e entrar mais rápido nas empresas mais desejadas.</Title>
+                </Column>
+                <Column>
+                    <Wrapper>
+                        <TitleLogin>Faça seu cadastro</TitleLogin>
+                        <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <Input 
+                                placeholder="E-mail" 
+                                leftIcon={<MdEmail />} 
+                                name="email"  
+                                control={control} 
+                                rules={{
+                                    required: 'E-mail é obrigatório',
+                                    pattern: {
+                                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                        message: 'E-mail inválido'
+                                    }
+                                }}
+                            />
+                            {errors.email && <span>{errors.email.message}</span>}
+                            <Input 
+                                type="password" 
+                                placeholder="Senha" 
+                                leftIcon={<MdLock />}  
+                                name="senha" 
+                                control={control} 
+                                rules={{
+                                    required: 'Senha é obrigatória',
+                                    minLength: { value: 6, message: 'Senha deve ter pelo menos 6 caracteres' }
+                                }}
+                            />
+                            {errors.senha && <span>{errors.senha.message}</span>}
+                            <Button title="Entrar" variant="secondary" type="submit" />
+                        </form>
+                        <Row>
+                            <EsqueciText>Esqueci minha senha</EsqueciText>
+                            <CriarText onClick={handleCreateAccount}>Criar Conta</CriarText>
+                        </Row>
+                    </Wrapper>
+                </Column>
+            </Container>
+        </>
+    );
+};
 
-export { Login }
+export { Login };
